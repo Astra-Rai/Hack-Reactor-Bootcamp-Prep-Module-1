f6970051d46024707f5aab82fa540c730e479040
Astra Rai <42603584+Astra-Rai@users.noreply.github.com>
2019-04-21 18:13:57 -0400

    055_getElementsUpTo

---
+++ 055_getElementsUpTo.js
@@ -0,0 +1,21 @@
+/*
+Write a function called "getElementsUpTo".
+
+Given an array and a index, "getElementsUpTo", returns an array with all the elements up until, but not including, the element at the given index.
+
+Notes:
+* In order to do this you should be familiar with the 'slice' method.
+
+var output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
+console.log(output); // --> ['a', 'b', 'c']
+*/
+
+function getElementsUpTo(array, n) {
+  // start at index 0
+  //select elements from array starting at index 0
+  //up to selected to but not including element represented by n
+  return array.slice(0, n);
+}
+
+var output = getElementsUpTo(['a', 'b', 'c', 'd', 'e'], 3) 
+console.log(output); // --> ['a', 'b', 'c']
